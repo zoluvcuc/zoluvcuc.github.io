7f23e121e9d1c100a39bfa06f12c32fbd87c448e
zoluvcuc <46480989+zoluvcuc@users.noreply.github.com>
2019-01-09 13:54:14 +0700

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,9 +115,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2019,
+                month: 2,
+                day: 23,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
